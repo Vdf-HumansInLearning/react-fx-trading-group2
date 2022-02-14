17cab1aa4a7e69d4f6575ddec8de07523c7ebcf0
Oana Giorgi <oana.giorgi@vodafone.com>
2022-02-14 14:57:35 +0200

    get CcyPairs from API

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -3,12 +3,34 @@ import useFetch from "./UseFetch";
 import '../styles/style-index.css'
 
 function Table() {
-    const { data: transactions, error, isPending } = useFetch(`http://localhost:8080/api/transactions`);
-
+    const { data: transactions, error, isPending } = useFetch(`http://localhost:8000/api/transactions`);
+    const { data: currencies } = useFetch(`http://localhost:8000/api/currencies/pairs`)
     return (
         <section className="col-sm-12 col-md-12 col-lg-6">
             <h5 className="color-titles">Blotter View</h5>
             <hr />
+            <div className="blotter-buttons">
+                <p className="subtitle">FILTERS</p>
+                <div className="vertical-line"></div><div className="row">
+                    {currencies && (
+                        <>
+                            <div className="input-group col mb-3">
+                                <label className="input-group-text" htmlFor="inputCcy">CCY Pair</label>
+                                <select className="form-select" id="inputCcy">
+                                    <option value="opt_none">Choose..</option>
+                                    {currencies.map((pair) => (
+                                        <option value={pair} id={pair} key={pair}>{pair}</option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div className="input-group col mb-3">
+                                <span className="input-group-text">Date</span>
+                                <input type="date" className="form-control" id="inputDateFilter" placeholder="12/02/2018" />
+                            </div>
+                        </>
+                    )}
+                </div>
+            </div>
             <div className="table-responsive">
                 {isPending && <div>Loading...</div>}
                 {error && <div>{error}</div>}
