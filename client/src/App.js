4057d7d1b94c5a3b141c6efdffee1a566a6d0d32
Costin Lungu <costin.lungu@vodafone.com>
2022-02-18 11:21:22 +0200

    Register validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Aside from "../components/Aside";
 import BtnsLoginRegister from "../components/BtnsLoginRegister";
@@ -11,6 +11,13 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repassword, setConfirmPass] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [error, setError] = useState({
+    username: "",
+    email: "",
+    password: "",
+    repassword: "",
+  });
 
   const [toast, setToast] = useState({
     isShown: false,
@@ -19,57 +26,129 @@ const Register = () => {
     toastType: "success",
   });
 
+  const validateUser = () => {
+    let usernameRegExp = /^[a-zA-Z ,.'-]{3,20}$/;
+    let emailRegExp =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    let passRegExp =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+    if (!usernameRegExp.test(username)) {
+      setIsValid(false);
+      setError({
+        username:
+          "Username should have between 3 and 20 characters. It cannot contain numbers",
+        email: "",
+        password: "",
+        repassword: "",
+      });
+    } else if (!email) {
+      setIsValid(false);
+      setError({
+        username: "",
+        email: "Email is required!",
+        password: "",
+        repassword: "",
+      });
+    } else if (!emailRegExp.test(email)) {
+      setIsValid(false);
+      setError({
+        username: "",
+        email: "Invalid! Email should contain '@' and a domain!",
+        password: "",
+        repassword: "",
+      });
+    } else if (!passRegExp.test(password)) {
+      setIsValid(false);
+      setError({
+        username: "",
+        email: "",
+        password:
+          "Invalid! Password must be 8 characters long and must contain at least: one uppercase, one lowercase, a number and a special character!",
+        repassword: "",
+      });
+    } else if (password !== repassword) {
+      setIsValid(false);
+      setError({
+        username: "",
+        email: "",
+        password: "",
+        repassword: "Password do not match!",
+      });
+    } else {
+      setIsValid(true);
+      setError({
+        username: "",
+        email: "",
+        password: "",
+        repassword: "",
+      });
+    }
+    return isValid;
+  };
+
+  const firstUpdate = useRef(true);
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    validateUser();
+  }, [username, email, password, repassword]);
+
   const handleRegister = (e) => {
     e.preventDefault();
-    let url = "http://localhost:8080/api/auth/register";
-    fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username: username,
-        email: email,
-        password: password,
-        repassword: repassword,
-      }),
-    })
-      .then((res) =>
-        res.json().then((data) => ({
-          status: res.status,
-          body: data,
-        }))
-      )
-      .then((data) => {
-        if (data.status === 200) {
-          setToast({
-            isShown: true,
-            toastTitle: "Registration success",
-            toastMessage: "You have been successfully registered!",
-            toastType: "success",
-          });
-          setTimeout(() => {
-            navigate("/dashboard");
-          }, 2000);
-        } else if (data.status === 409) {
-          if (data.body.existing === "username") {
-            setToast({ isShown: true, toastMessage: data.body.message });
-            setTimeout(() => {
-              setToast({ isShown: false });
-            }, 2000);
-          } else if (data.body.existing === "email") {
-            setToast({ isShown: true, toastMessage: data.body.message });
-            setTimeout(() => {
-              setToast({ isShown: false });
-            }, 2000);
-          }
-        } else {
-          setToast(true, "Error", "Registration failed.", "fail");
-        }
+    if (isValid) {
+      let url = "http://localhost:8080/api/auth/register";
+      fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          email: email,
+          password: password,
+          repassword: repassword,
+        }),
       })
-      .catch((error) => {
-        console.log(error);
-      });
+        .then((res) =>
+          res.json().then((data) => ({
+            status: res.status,
+            body: data,
+          }))
+        )
+        .then((data) => {
+          if (data.status === 200) {
+            setToast({
+              isShown: true,
+              toastTitle: "Registration success",
+              toastMessage: "You have been successfully registered!",
+              toastType: "success",
+            });
+            setTimeout(() => {
+              navigate("/dashboard");
+            }, 2000);
+          } else if (data.status === 409) {
+            if (data.body.existing === "username") {
+              setToast({ isShown: true, toastMessage: data.body.message });
+              setTimeout(() => {
+                setToast({ isShown: false });
+              }, 2000);
+            } else if (data.body.existing === "email") {
+              setToast({ isShown: true, toastMessage: data.body.message });
+              setTimeout(() => {
+                setToast({ isShown: false });
+              }, 2000);
+            }
+          } else {
+            setToast(true, "Error", "Registration failed.", "fail");
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
   return (
@@ -98,6 +177,7 @@ const Register = () => {
                 required
                 onChange={(e) => setUsername(e.target.value)}
               />
+              <div id="error-modal">{error?.username}</div>
             </div>
             <div className="mb-3 align-content-center">
               <p>Email adress</p>
@@ -110,6 +190,7 @@ const Register = () => {
                 placeholder="email@fxtrading.com"
                 onChange={(e) => setEmail(e.target.value)}
               />
+              <div id="error-modal">{error?.email}</div>
             </div>
             <div className="mb-3 align-content-center">
               <p>Password</p>
@@ -122,6 +203,7 @@ const Register = () => {
                 placeholder="Password123!"
                 onChange={(e) => setPassword(e.target.value)}
               />
+              <div id="error-modal">{error?.password}</div>
             </div>
             <div className="mb-3 align-content-center">
               <p>Confirm Password</p>
@@ -134,6 +216,7 @@ const Register = () => {
                 placeholder="Password123!"
                 onChange={(e) => setConfirmPass(e.target.value)}
               />
+              <div id="error-modal">{error?.repassword}</div>
             </div>
             <BtnsLoginRegister
               textContent={"Register"}
