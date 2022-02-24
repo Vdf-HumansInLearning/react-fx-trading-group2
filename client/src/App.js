a8c2fc28e35018c4193a0651d591cc1b3d99bef2
Oana Giorgi <oana.giorgi@vodafone.com>
2022-02-24 11:10:53 +0200

    imported necessary library

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Navigate, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
